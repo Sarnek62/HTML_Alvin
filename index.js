d83715f3db9dfeb8e08d739d5f37e5eff561c013
Baptiste Carreau <batcarreau@gmail.com>
2018-03-12 15:17:12 +0100

    Bouton violent

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function sayHello()
 {
   alert("Hello !")
 }
+
+function sayBadThings()
+{
+  alert("Ce bouton n'est pas gentil, il dit que ta mère suce des queues en enfer en compagnie de ta grand mère paternelle la tchoin")
+}
